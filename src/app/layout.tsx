bdff60696f9bc1b5bc5d1d2e77274c46f155fe83
Deren Somsanith <derensomsanith@gmail.com>
2023-08-29 17:41:12 -0500

    set up clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import "@/styles/globals.css";
 
+import { ClerkProvider } from "@clerk/nextjs";
+
 export const metadata = {
   title: "Deren Store",
   description: "Deren Store, I sell stuff that make you think about your ex",
@@ -11,10 +13,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <>
-        <>{children}</>
-      </>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
