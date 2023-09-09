1e9dbbe19b35bab897760a1ffe67aa03b1d6199d
Deren Somsanith <derensomsanith@gmail.com>
2023-09-09 13:34:37 -0500

    - finish store switcher

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,30 @@
 import { MainNav } from "@/components/MainNav";
-import { UserButton } from "@clerk/nextjs";
+import { StoreSwitcher } from "@/components/StoreSwitcher";
+import { prisma } from "@/server/db";
+import { UserButton, auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 type NavbarProps = {
   id?: string;
 };
 
-export const Navbar: React.FC<NavbarProps> = () => {
+export const Navbar: React.FC<NavbarProps> = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const stores = await prisma.store.findMany({
+    where: {
+      userId,
+    },
+  });
+
   return (
     <div className="border-b ">
       <div className="flex h-16 items-center px-4">
-        <div>This will be a store switcher</div>
+        <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
           <UserButton afterSignOutUrl="/" />
