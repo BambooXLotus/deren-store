34210817e4c1cd09916d1d5d81286595a616f855
Deren Somsanith <derensomsanith@gmail.com>
2023-09-11 17:59:48 -0500

    add comment for typescript

--- src/hooks/use-origin.tsx
+++ src/hooks/use-origin.tsx
@@ -4,6 +4,8 @@ export const useOrigin = () => {
   const [mounted, setMounted] = useState(false);
 
   const origin =
+    //Optional chaining does not work for this!
+    // eslint-disable-next-line @typescript-eslint/prefer-optional-chain
     typeof window !== "undefined" && window.location.origin
       ? window.location.origin
       : "";
